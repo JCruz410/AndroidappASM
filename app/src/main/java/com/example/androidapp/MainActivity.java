8552fd922b5ab37e571b723a5e6485c787aa3cab
Joel Cruz <joel_cruz410@yahoo.com>
2022-06-28 16:38:44 -0400

    Replaced Progress bar login with IP address requirement.

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -10,28 +10,5 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar progressBar;
-    int count = 0;
-    Timer timer;
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        progressBar = findViewById(R.id.progressbar);
-        timer = new Timer();
-        TimerTask timerTask = new TimerTask() {
-            @Override
-            public void run() {
-                count++;
-                progressBar.setProgress(count);
-                if (count == 100) {
-                    timer.cancel();
-                }
-            }
-        };
-        timer.schedule(timerTask,0,100);
-    }
 }
