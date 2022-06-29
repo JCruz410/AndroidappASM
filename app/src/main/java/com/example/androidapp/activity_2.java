eb7bf32347468b76a66b6e86aaeaf095980b0023
Joel Cruz <joel_cruz410@yahoo.com>
2022-06-28 20:29:27 -0400

    IP address login established 95% (crash on login)

--- app/src/main/java/com/example/androidapp/activity_2.java
+++ app/src/main/java/com/example/androidapp/activity_2.java
@@ -1,14 +1,94 @@
 package com.example.androidapp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import static com.example.androidapp.activity_1.ip_address;
 
 public class activity_2 extends AppCompatActivity {
+
+    TextView txvalue;
+    Handler handler = new Handler();
+    boolean statusdevice = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-    }
-}
 
+
+        handler.postDelayed(status_data,0);
+
+    }
+
+    private Runnable status_data = new Runnable() {
+        @Override
+        public void run() {
+            if (statusdevice) {
+                request_to_url("");
+                handler.postDelayed(this, 2000);
+                Log.d("Status", "Connectivity_esp32");
+            }else {
+                handler.removeCallbacks(status_data);
+                Log.d("Status","Finalized");
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        statusdevice = false;
+    }
+
+    public void request_to_url (String command) {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected()) {
+
+            new request_data().execute("http://" + ip_address + "/" + command);
+
+        }else {
+            Toast.makeText(activity_2.this, "Not connected  ", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    private class request_data extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            return Connectivity.geturl(url[0]);
+
+        }
+
+        @Override
+        protected void onPostExecute(String result_data) {
+            if(result_data !=null) {
+                txvalue.setText(result_data);
+
+            }else{
+
+                Toast.makeText(activity_2.this, "Null data", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+}
