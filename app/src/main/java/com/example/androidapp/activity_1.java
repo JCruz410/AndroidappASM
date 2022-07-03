2a5cf9071d52c28d98dda2767724cf7765532b8f
Joel Cruz <joel_cruz410@yahoo.com>
2022-07-03 15:39:40 -0400

    minor update/fix front end

--- app/src/main/java/com/example/androidapp/activity_1.java
+++ app/src/main/java/com/example/androidapp/activity_1.java
@@ -2,9 +2,16 @@ package com.example.androidapp;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -17,7 +24,14 @@ public static String ip_address;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_1);
+
+
+
 
         ip = (EditText) findViewById(R.id.edit_ip_address);
         enter_ip = (Button) findViewById(R.id.button_ip);
@@ -25,16 +39,18 @@ public static String ip_address;
         enter_ip.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick (View v)
-            {
-                ip_address = ip.getText().toString();
-                Intent intent = new Intent(activity_1.this, activity_2.class);
+            public void onClick (View v) {
+                ip_address = ip.getText().toString().trim();
+                if (ip_address.isEmpty()) {
+                    ip.setError("wrong ip");
+                } else {
+                    Intent intent = new Intent(activity_1.this, activity_2.class);
 
-                startActivity(intent);
+                    startActivity(intent);
 
+                }
             }
         });
-
     }
 
 }
