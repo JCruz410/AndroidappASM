8d0b9423c8e768835b5faa54dfe6634ddd1c4275
Joel Cruz <joel_cruz410@yahoo.com>
2022-07-18 16:10:20 -0400

    Ansync requests setup/still have to fix calling all requests at the same time

--- app/src/main/java/com/example/androidapp/Connectivity.java
+++ app/src/main/java/com/example/androidapp/Connectivity.java
@@ -3,6 +3,7 @@ package com.example.androidapp;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -19,39 +20,28 @@ import androidx.annotation.NonNull;
 public class Connectivity {
     public static String geturl (String url_esp32){
         OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(url_esp32)
-                .build();
-
+        Request request = new Request.Builder().url(url_esp32).build();
+        client.dispatcher().setMaxRequestsPerHost(20);
         client.newCall(request).enqueue(new Callback() {
-            @Override public void onFailure(Call call, IOException e) {
+            @Override
+            public void onFailure(Call call, IOException e) {
                 e.printStackTrace();
             }
 
-            @Override public void onResponse(Call call, Response response) throws IOException {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
                 try (ResponseBody responseBody = response.body()) {
-                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                    if (!response.isSuccessful())
+                        throw new IOException("Unexpected code " + response);
 
                     Headers responseHeaders = response.headers();
                     for (int i = 0, size = responseHeaders.size(); i < size; i++) {
                         System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
                     }
-
-                    System.out.println(responseBody.string());
+                        responseBody.string();
                 }
             }
         });
-
-        try
-        {
-            Response response = client.newCall(request).execute();
-            return response.body().string();
-
-        } catch(IOException error) {
-
-            return error.toString();
-
-        }
-
+        return "";
     }
 }
